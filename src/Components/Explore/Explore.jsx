9269d61829881d6f744073aa7792f560983d2ad2
AshwinR27 <86276308+AshwinR27@users.noreply.github.com>
2023-06-28 12:34:18 +0530

    projects part work

--- src/Components/Explore/Explore.jsx
+++ src/Components/Explore/Explore.jsx
@@ -6,23 +6,24 @@ import { useLoaderData } from 'react-router-dom';
 
 
 export default function Explore() {
-    const educationArr = useLoaderData()
+    const educationArr = useLoaderData()[0]
 
-    const [explore, setExplore] = React.useState(false)
+    const [explore, setExplore] = React.useState(true)
 
     const toggleExplore = () => {
         setExplore(prevExplore => !prevExplore)
     }
 
     const mappedEducationData = educationArr.map(data => (
-       
+
+            
             <div className='education-details'>
                 <h1>{data.course}</h1>
                 <h2>{data.instituteName}</h2>
                 <h3>{data.yearRange}</h3>
                 <h4>{data.score}</h4>
             </div>
-            
+      
     ))
 
     return ( 
